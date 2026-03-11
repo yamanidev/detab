04a677a6b9d4d35dce6e7307e0101360217359e1
yamanidev <elamine.yamani@gmail.com>
2026-03-11 14:20:34 +0100

    refactor: copy `manifest.json` to `dist/` through the Vite lifecycle hook `closeBundle`

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,17 @@
 import { defineConfig } from "vite";
+import { copyFileSync } from "fs";
 
 export default defineConfig({
   base: "",
   publicDir: false,
+  plugins: [
+    {
+      name: "copy-manifest",
+      closeBundle() {
+        copyFileSync("manifest.json", "dist/manifest.json");
+      },
+    },
+  ],
   build: {
     outDir: "dist",
     emptyOutDir: true,
